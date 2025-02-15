2863953683b4172f87aa9a9b8377c463104dfae2
LisaTho <lisathoennessen@gmail.com>
2025-02-15 06:52:20 +0100

    Correcting space counter

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 def main(): #execute functions to locate and open the text and count its words
     path = get_path()
     book = read_book(path)
-    count, words = list_of(book)
-    count = symbol_count(count, words)
+    count = symbol_count(book)
     print(count)
 
 def get_path(): #return the file location
@@ -13,13 +12,9 @@ def read_book(loc): #read the file and return its content as a string with lower
         book = f.read()
     return book.lower()
 
-def list_of(b): #seperates the string through spaces and returns the length of the resulting list and the list itself
-    string = b.split()
-    return len(string), string
-
-def symbol_count(c, w): #count the number of each symbol in the list of words and return as a dictionary
-    count = {" ": c-1}
-    for word in w:
+def symbol_count(s): #count the number of each symbol in the list of words and return as a dictionary
+    count = {}
+    for word in s:
         for char in word:
             if char not in count:
                 count[char] = 1
