a9f9954a905b03915f649705af93a881479f6bf6
LisaTho <lisathoennessen@gmail.com>
2025-02-15 06:41:44 +0100

    Adding symbol counter functionality

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 def main(): #execute functions to locate and open the text and count its words
     path = get_path()
     book = read_book(path)
-    words = count(book)
-    print(words)
+    count, words = list_of(book)
+    count = symbol_count(count, words)
+    print(count)
 
 def get_path(): #return the file location
     return "books/frankenstein.txt"
 
-def read_book(loc): #read the file and return its content as a string
+def read_book(loc): #read the file and return its content as a string with lower case
     with open(loc) as f:
-        return f.read()
+        book = f.read()
+    return book.lower()
 
-def count(b): #seperates the string through spaces and returns the length of the resulting list
+def list_of(b): #seperates the string through spaces and returns the length of the resulting list and the list itself
     string = b.split()
-    # s = [string[i] for i in range(10)]
-    # print(s)
-    return len(string)
+    return len(string), string
+
+def symbol_count(c, w): #count the number of each symbol in the list of words and return as a dictionary
+    count = {" ": c-1}
+    for word in w:
+        for char in word:
+            if char not in count:
+                count[char] = 1
+            else:
+                count[char] += 1
+    return count
 
 main()
